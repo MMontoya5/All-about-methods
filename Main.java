4493a4f4d0d8ca46b5c960e92b88a1928b1dc13a
MMontoya5 <mateo.montoya@patelhs.org>
2021-01-29 13:34:33 +0000

    Added question for user

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ class Main
 {
   public static void main(String[] args) 
   {
+    Scanner scan = new Scanner(System.in);
     
-  }
+     System.out.println("What is the max amount of people that can fit in the park? (Whole # only)");
+    
+
+
+
+
 }
