9df37a45f555b4825abb78a737021a44fbc3bac4
MMontoya5 <mateo.montoya@patelhs.org>
2021-01-29 13:35:52 +0000

    Added another question

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ class Main
     
      System.out.println("What is the max amount of people that can fit in the park? (Whole # only)");
     
+    int numMax = scan.nextInt();
 
-
-
+    System.out.println("How many people do you see at the park? (Whole # only)");
+  
 
 }
