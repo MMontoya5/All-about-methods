1dd337e6f9df8816ec319dc85abb84112ae47759
MMontoya5 <mateo.montoya@patelhs.org>
2021-02-01 17:52:34 +0000

    Added my method

--- Main.java
+++ Main.java
@@ -5,11 +5,24 @@ class Main
   {
     Scanner scan = new Scanner(System.in);
     
+    //Asking for the max amount of people that can go to the park
      System.out.println("What is the max amount of people that can fit in the park? (Whole # only)");
     
     int numMax = scan.nextInt();
 
     System.out.println("How many people do you see at the park? (Whole # only)");
   
+    int numPeople = scan.nextInt();
 
+  System.out.println("There are "  + spaceAvailable(numPeople, numMax) + " spaces left");
+  
+  
+ }
+  
+   static int spaceAvailable(int numPeople, int numMax)
+  {
+    return numMax + numPeople;
+  }
+   
+ 
 }
